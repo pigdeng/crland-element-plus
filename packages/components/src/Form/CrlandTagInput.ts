ff8a082181787801b4a75133816870cb6a3f41c5
bigdeng <ok@bigdeng.com>
2022-09-27 16:16:34 +0800

    update

--- packages/components/src/Form/CrlandTagInput.ts
+++ packages/components/src/Form/CrlandTagInput.ts
@@ -1,12 +1,32 @@
 import { ExtractPropTypes } from "vue";
 export const size = ["large", "default", "small"];
+export const tagType = [
+  "primary",
+  "success",
+  "info",
+  "warning",
+  "danger",
+  "text",
+];
 export const propsData = {
+  value: {
+    type: Array,
+    default: () => {
+      return [];
+    },
+  },
   size: {
     type: String,
     validator(value: string) {
       return size.includes(value);
     },
-    default: "default",
+  },
+  tagType: {
+    type: [String, undefined],
+    validator(value: string) {
+      return tagType.includes(value);
+    },
+    default: "info",
   },
 };
 export type propsData = ExtractPropTypes<typeof propsData>;
