7a6fa501e8e7f9c2be1f43d4c1af4cbacc7f6c0b
DaiSQ <anki@ankideMacBook-Pro.local>
2022-10-12 09:24:11 +0800

    重新封装

--- examples/src/demo/navigation/CrlandBreadcrumb.vue
+++ examples/src/demo/navigation/CrlandBreadcrumb.vue
@@ -1,8 +1,43 @@
 <template>
-    <crland-breadcrumb></crland-breadcrumb>
+    <el-breadcrumb :separator-icon="ArrowRight">
+      <el-breadcrumb-item :to="{ path: '/' }" >
+        <el-icon>
+          <HomeFilled />
+        </el-icon>
+      </el-breadcrumb-item>
+      <el-breadcrumb-item>promotion management</el-breadcrumb-item>
+      <el-breadcrumb-item>promotion list</el-breadcrumb-item>
+      <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+    </el-breadcrumb>
 </template>
 
 <script lang="ts" setup>
-//import {  } from "crland-element-plus";
-
+import { ArrowRight,HomeFilled } from '@element-plus/icons-vue'
+import { ElBreadcrumb, ElBreadcrumbItem } from "element-plus";
 </script>
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
